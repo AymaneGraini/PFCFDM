d025fe2ddc25119bbecfc10a99b3dc308d365c04
Aymane.Graini <aymane.graini@polytechnique.edu>
2025-06-07 09:13:00 +0200

    NonPeriodic convolution with pads

---
+++ PFCproc_TODO/ProcessPFC_padFFT.py
@@ -0,0 +1,382 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import jax.numpy as jnp
+import jax
+from jax import tree_util
+from functools import partial
+
+def Jcompute_gradient(scalar_field, ax, sp):
+    return jnp.gradient(scalar_field, sp, axis=ax)
+
+def Jgradient_periodic(array, spacing, axis):
+    return jnp.gradient(array, spacing, axis=axis)
+
+# def Jspectral_filter(shape,dx,dy):
+#         x = jnp.fft.fftfreq(shape[1],dx)  
+#         y = jnp.fft.fftfreq(shape[0],dy) 
+#         KX,KY=jnp.meshgrid(x,y)
+#         radius_squared = KX**2 + KY**2
+
+#         # g = jnp.exp(-40*(
+#         #         (KX*dx)**2+
+#         #         (KY*dy)**2
+#         #         ))
+#         g = jnp.exp(-2 * (jnp.pi**2) * sigma**2 * radius_squared)
+#         return g
+
+def JGkernel_jax(shape, dx, dy, sigma):
+    wx = jnp.fft.fftfreq(shape[1], dx)  
+    wy = jnp.fft.fftfreq(shape[0], dy)  #
+    KX, KY = jnp.meshgrid(wx, wy)
+    radius_squared = KX**2 + KY**2
+    kernel = jnp.exp(-2 * (jnp.pi**2) * sigma**2 * radius_squared)
+    return kernel
+
+def levi_civita(i, j, k):
+    """Returns the Levi-Civita symbol e_{ijk}."""
+    return np.sign(np.linalg.det([[i == 0, j == 0, k == 0],
+                                  [i == 1, j == 1, k == 1],
+                                  [i == 2, j == 2, k == 2]]))
+Levi= np.zeros((3,3,3))
+for i in range(3):
+    for j in range(3):
+        for k in range(3):
+            Levi[i,j,k] = levi_civita(i, j, k)
+
+
+
+
+#TODO The class is not clean and it repeats many functions
+filterQ = True
+
+class PFCProcessor():
+    def __init__(
+        self,
+        space_coords,
+        DofMap,
+        qs ,
+        a0,
+        target_shape : tuple,
+        pads : tuple,
+    ) -> None:
+        
+        self.space_coords= space_coords
+        self.a0=a0
+        self.qs= jnp.array(qs)
+        self.DofMap = DofMap
+        self.target_shape=target_shape
+        self.coords =space_coords[:,:2][DofMap]
+
+        #Build the reverse map
+        self.rev_DofMap = jnp.empty_like(self.DofMap)
+        self.rev_DofMap = self.rev_DofMap.at[DofMap].set(jnp.arange(len(DofMap)))
+
+        x_coords =jnp.unique(self.coords[:, 0],size= target_shape[1])
+        y_coords =jnp.unique(self.coords[:, 1],size= target_shape[0])
+        self.X, self.Y = jnp.meshgrid(x_coords, y_coords)
+        self.L = self.X.max() - self.X.min()
+        self.H = self.Y.max() - self.Y.min()
+        self.nx = self.X.shape[1]
+        self.ny = self.X.shape[0]
+        self.dx = self.L / (self.target_shape[1] - 1) 
+        self.dy = self.H / (self.target_shape[0]- 1)  
+
+        self.padx, self.pady = pads
+        self.pad_shape = (2*self.target_shape[0], 2*self.target_shape[1])
+        kernel =  np.exp(-((self.X-self.L/2)**2 + (self.Y-self.H/2)**2) / (2 * self.a0**2))
+        kernel /= np.sum(kernel)
+
+        self.AmpKernel = np.zeros(self.pad_shape)
+
+        start_x = (self.pad_shape[0] - self.target_shape[0]) // 2
+        start_y = (self.pad_shape[1] - self.target_shape[1]) // 2
+
+        self.AmpKernel[start_x:start_x + self.target_shape[0],
+                    start_y:start_y + self.target_shape[1]] = kernel
+
+
+        self.AmpKernel=jnp.fft.fft2(self.AmpKernel)
+        
+        self.AmpKernel = JGkernel_jax(self.pad_shape,self.dx,self.dy,self.a0)
+
+        self.filterQ = True
+        kx = jnp.fft.fftfreq(self.nx,self.dx)*2*jnp.pi
+        ky = jnp.fft.fftfreq(self.ny,self.dy) *2*jnp.pi
+        KX,KY=jnp.meshgrid(kx,ky)
+        self.K_vec = jnp.stack((KX, KY), axis=-1)
+        self.FF = []  
+        for i in range(len(qs)):
+            q = self.qs[i]
+            f = lambda x, y: jnp.exp(-1j * (q[0] * x + q[1] * y))
+            self.FF.append(f(self.X, self.Y))  
+        self.FF=jnp.array(self.FF) 
+        self.filterQ = True
+        
+    # @partial(jax.jit, static_argnames=['self'])
+    def C_Amp(self,op,i):
+        #op is called as a flattened out ndarray coming from dolfinx
+        psi = jnp.array(op[self.DofMap].reshape(self.target_shape))
+        # padded = np.pad(
+        #     psi,
+        #     pad_width=((self.padx, self.padx), (self.pady, self.pady)),
+        #     mode='wrap'
+        # )
+    
+        start_x = (self.pad_shape[0] - self.target_shape[0]) // 2
+        start_y = (self.pad_shape[1] - self.target_shape[1]) // 2
+
+        field_padded = np.zeros(self.pad_shape,dtype=np.complex64)
+
+        field_padded[start_x:start_x + self.target_shape[0],
+                    start_y:start_y + self.target_shape[1]] = psi*self.FF[i]
+        
+     
+
+        image_fft = jnp.fft.fft2(field_padded)
+        convolved_fft = image_fft * self.AmpKernel
+        convolved = jnp.fft.ifft2(convolved_fft)
+        # plt.imshow(np.absolute(convolved[start_x:start_x + self.target_shape[0],
+        #             start_y:start_y + self.target_shape[1]] ),origin='lower',cmap="s²eismic")
+        # plt.show()
+        return convolved[start_x:start_x + self.target_shape[0],
+                    start_y:start_y + self.target_shape[1]]
+        
+    # @partial(jax.jit, static_argnames=['self'])
+    def Compute_Q(self,amps):
+        def compute_single_q(n):
+            An = amps[n]
+            grad_x = Jcompute_gradient(An, 1, self.dx)
+            grad_y = Jcompute_gradient(An, 0, self.dy)
+            nabla_An = jnp.zeros((self.ny, self.nx, 2), dtype=jnp.complex64).at[:, :, 0].set(grad_x / An).at[:, :, 1].set(grad_y / An)
+            holder = jnp.imag(nabla_An)
+            q_field = jnp.broadcast_to(self.qs[n], (self.ny, self.nx, 2))
+            return (2 / 6) * jnp.einsum('ijk,ijl->ijkl', q_field, holder, optimize=True)
+        vecs = np.arange(0,len(self.qs),1)
+        Qcomp = jnp.sum(jax.vmap(compute_single_q)(vecs), axis=0)
+        if not self.filterQ:
+            return Qcomp.reshape(-1,4)[self.rev_DofMap].ravel()
+        else:
+            start_x = (self.pad_shape[0] - self.target_shape[0]) // 2
+            start_y = (self.pad_shape[1] - self.target_shape[1]) // 2
+            sp = JGkernel_jax(self.pad_shape,self.dx,self.dy,self.a0/4)
+            convQ = np.zeros_like(Qcomp)
+            for i in range(2):
+                for j in range(2):
+                    field_padded = np.zeros(self.pad_shape,dtype=np.complex64)
+                    field_padded[start_x:start_x + self.target_shape[0],
+                                        start_y:start_y + self.target_shape[1]] = Qcomp[:,:,i,j]
+                    fft_Q = jnp.fft.fft2(field_padded)
+                    convQ[:,:,i,j] = jnp.fft.ifft2(fft_Q*sp, axes=(0, 1)).real[start_x:start_x + self.target_shape[0],
+                                        start_y:start_y + self.target_shape[1]]
+            return convQ.reshape(-1,4)[self.rev_DofMap].ravel()
+
+
+    # @partial(jax.jit, static_argnames=['self'])
+    def Compute_alpha(self,Qt):
+        #Qt is called as a flattened out ndarray coming from dolfinx
+        Qcomp= Qt.reshape(-1,4)[self.DofMap].reshape(self.ny,self.nx,2,2)
+        Nx, Ny = Qcomp.shape[1], Qcomp.shape[0]
+        curl = jnp.zeros((Nx, Ny,3,3))
+        grad_x = Jgradient_periodic(Qcomp, self.dy, 1)
+        grad_y = Jgradient_periodic(Qcomp, self.dx , 0)
+        derivative_array = jnp.zeros((self.ny, self.nx,2,2,2)).at[:,:,0,:,:].set(grad_x).at[:,:,1,:,:].set(grad_y)
+        padded_array = jnp.pad(derivative_array, ((0, 0), (0, 0), (0, 1), (0, 1), (0, 1)), mode='constant', constant_values=0)
+        curl = jnp.einsum('jkl,abkil->abij', Levi,padded_array,optimize=True)
+        return curl.reshape(-1,9)[self.rev_DofMap].ravel()
+
+    # @partial(jax.jit, static_argnames=['self'])
+    def Analytical_gradFuq(self,Qt,Ut,amps):
+        Q= Qt.reshape(-1,4)[self.DofMap].reshape(self.ny,self.nx,2,2)
+        U= Ut.reshape(-1,4)[self.DofMap].reshape(self.ny,self.nx,2,2)
+        Diff = Q-U
+        def Single_vec_con(n):
+            q=self.qs[n]
+            An = amps[n]
+            grad_x = Jcompute_gradient(An, 1, self.dx)
+            grad_y = Jcompute_gradient(An, 0, self.dy)
+            A_term = jnp.zeros((self.ny, self.nx, 2), dtype=jnp.complex64).at[:, :, 0].set((grad_x / (An)**2)).at[:, :, 1].set((grad_y / (An)**2))
+            gradF_dAm=jnp.zeros((self.ny, self.nx))
+            for i in range(2):
+                for j in range(2):
+                    gradF_dAm += q[i]*self.FF[n]*jnp.fft.ifft2((jnp.fft.fft2(Diff[:,:,i,j]*A_term[:,:,j])+1j*self.K_vec[:,:,j]*jnp.fft.fft2(Diff[:,:,i,j]*1/An))*self.AmpKernel)
+            return  gradF_dAm
+        vecs = np.arange(0,len(self.qs),1)
+        TA= jnp.sum(jax.vmap(Single_vec_con)(vecs), axis=0)
+        return (2/6)*TA.imag.ravel()[self.rev_DofMap]
+
+
+    def Coarse_grain(self,R):
+        """
+        Retreives a coarsegrained version or the tensor R
+        """
+        sp = JGkernel_jax(self.target_shape,self.dx,self.dy,self.a0*0.75)
+        Rcomp= R.reshape(-1,4)[self.DofMap].reshape(self.ny,self.nx,2,2)
+        fft_R = jnp.fft.fft2(Rcomp, axes=(0, 1))
+        filtered_fft_R = jnp.einsum('xyij,xy->xyij', fft_R, sp, optimize=True)
+        convR = jnp.fft.ifft2(filtered_fft_R, axes=(0, 1)).real
+        return convR.reshape(-1,4)[self.rev_DofMap].ravel()
+    
+def Compute_Q_jax(FE_psi,proc):
+    """
+        Computes Q based on psi using Jax:
+        FE_psi is un unordered array coming for dolfinx. It's reordred into psi
+        Returns a flattened out array in the same order as that of FE.
+    """
+    psi = jnp.array(FE_psi[proc.DofMap].reshape(proc.target_shape))
+    # Convert qs to a JAX array outside the inner function
+    qs_array = jnp.array(proc.qs)
+    
+    def compute_single_q(n):
+        q = qs_array[n]  
+
+        # Exponential modulation
+        f = lambda x, y: jnp.exp(-1j * (q[0] * x + q[1] * y))
+        FF = f(proc.X, proc.Y)
+
+        start_x = (proc.pad_shape[0] - proc.target_shape[0]) // 2
+        start_y = (proc.pad_shape[1] - proc.target_shape[1]) // 2
+
+        field_padded = jnp.zeros(proc.pad_shape,dtype=np.complex64)
+
+        field_padded= field_padded.at[start_x:start_x + proc.target_shape[0],
+                    start_y:start_y + proc.target_shape[1]].set(psi*FF)
+        
+     
+
+        image_fft = jnp.fft.fft2(field_padded)
+        convolved_fft = image_fft * proc.AmpKernel
+        convolved = jnp.fft.ifft2(convolved_fft)
+        An = convolved[start_x:start_x + proc.target_shape[0],
+                    start_y:start_y + proc.target_shape[1]]
+
+        # Approximate gradient using finite differences manually, since jnp.gradient doesn't work with tracers
+        grad_x = jnp.gradient(An, proc.dx, axis=1)
+        grad_y = jnp.gradient(An, proc.dy, axis=0)
+
+        # Build gradient field normalized by An
+        nabla_An = jnp.zeros((proc.ny, proc.nx, 2), dtype=jnp.complex64)
+        nabla_An = nabla_An.at[:, :, 0].set(grad_x / An)
+        nabla_An = nabla_An.at[:, :, 1].set(grad_y / An)
+
+        # Imaginary part of normalized gradient field
+        holder = jnp.imag(nabla_An)
+
+        # Broadcast q vector across field
+        q_field = jnp.broadcast_to(q, (proc.ny, proc.nx, 2))
+
+        # Tensor product and scale
+        return (2 / 6) * jnp.einsum('ijk,ijl->ijkl', q_field, holder, optimize=True)
+
+    vecs = jnp.arange(len(proc.qs))
+
+    Qcomp = jnp.sum(jax.vmap(compute_single_q)(vecs), axis=0)
+    start_x = (proc.pad_shape[0] - proc.target_shape[0]) // 2
+    start_y = (proc.pad_shape[1] - proc.target_shape[1]) // 2
+    sp = JGkernel_jax(proc.pad_shape,proc.dx,proc.dy,proc.a0/4)
+    # fft_Q = jnp.fft.fft2(Q, axes=(0, 1))
+    # filtered_fft_Q = jnp.einsum('xyij,xy->xyij', fft_Q, sp, optimize=True)
+    # convQ = jnp.fft.ifft2(filtered_fft_Q, axes=(0, 1)).real
+    convQ = jnp.zeros_like(Qcomp)
+    for i in range(2):
+        for j in range(2):
+                    field_padded = jnp.zeros(proc.pad_shape,dtype=np.complex64)
+                    field_padded= field_padded.at[start_x:start_x + proc.target_shape[0],
+                                        start_y:start_y + proc.target_shape[1]].set(Qcomp[:,:,i,j])
+                    fft_Q = jnp.fft.fft2(field_padded)
+                    convQ = convQ.at[:,:,i,j].set(jnp.fft.ifft2(fft_Q*sp, axes=(0, 1)).real[start_x:start_x + proc.target_shape[0],
+                                        start_y:start_y + proc.target_shape[1]])
+    # return convQ
+    return convQ.reshape(-1,4)[proc.rev_DofMap].ravel()
+
+def Compute_Q_sym_jax(FE_psi,proc):
+    """
+        Computes Q based on psi using Jax:
+        FE_psi is un unordered array coming for dolfinx. It's reordred into psi
+        Returns a flattened out array in the same order as that of FE.
+    """
+    psi = jnp.array(FE_psi[proc.DofMap].reshape(proc.target_shape))
+    # Convert qs to a JAX array outside the inner function
+    qs_array = jnp.array(proc.qs)
+    
+    def compute_single_q(n):
+        q = qs_array[n]  
+
+        # Exponential modulation
+        f = lambda x, y: jnp.exp(-1j * (q[0] * x + q[1] * y))
+        FF = f(proc.X, proc.Y)
+
+        start_x = (proc.pad_shape[0] - proc.target_shape[0]) // 2
+        start_y = (proc.pad_shape[1] - proc.target_shape[1]) // 2
+
+        field_padded = jnp.zeros(proc.pad_shape,dtype=np.complex64)
+
+        field_padded= field_padded.at[start_x:start_x + proc.target_shape[0],
+                    start_y:start_y + proc.target_shape[1]].set(psi*FF)
+        
+     
+
+        image_fft = jnp.fft.fft2(field_padded)
+        convolved_fft = image_fft * proc.AmpKernel
+        convolved = jnp.fft.ifft2(convolved_fft)
+        An = convolved[start_x:start_x + proc.target_shape[0],
+                    start_y:start_y + proc.target_shape[1]]
+
+        # Approximate gradient using finite differences manually, since jnp.gradient doesn't work with tracers
+        grad_x = jnp.gradient(An, proc.dx, axis=1)
+        grad_y = jnp.gradient(An, proc.dy, axis=0)
+
+        # Build gradient field normalized by An
+        nabla_An = jnp.zeros((proc.ny, proc.nx, 2), dtype=jnp.complex64)
+        nabla_An = nabla_An.at[:, :, 0].set(grad_x / An)
+        nabla_An = nabla_An.at[:, :, 1].set(grad_y / An)
+
+        # Imaginary part of normalized gradient field
+        holder = jnp.imag(nabla_An)
+
+        # Broadcast q vector across field
+        q_field = jnp.broadcast_to(q, (proc.ny, proc.nx, 2))
+
+        # Tensor product and scale
+        return (2 / 6) * jnp.einsum('ijk,ijl->ijkl', q_field, holder, optimize=True)
+
+    vecs = jnp.arange(len(proc.qs))
+
+    Qcomp = jnp.sum(jax.vmap(compute_single_q)(vecs), axis=0)
+    start_x = (proc.pad_shape[0] - proc.target_shape[0]) // 2
+    start_y = (proc.pad_shape[1] - proc.target_shape[1]) // 2
+    sp = JGkernel_jax(proc.pad_shape,proc.dx,proc.dy,proc.a0/4)
+    # fft_Q = jnp.fft.fft2(Q, axes=(0, 1))
+    # filtered_fft_Q = jnp.einsum('xyij,xy->xyij', fft_Q, sp, optimize=True)
+    # convQ = jnp.fft.ifft2(filtered_fft_Q, axes=(0, 1)).real
+    convQ = jnp.zeros_like(Qcomp)
+    for i in range(2):
+        for j in range(2):
+                    field_padded = jnp.zeros(proc.pad_shape,dtype=np.complex64)
+                    field_padded= field_padded.at[start_x:start_x + proc.target_shape[0],
+                                        start_y:start_y + proc.target_shape[1]].set(Qcomp[:,:,i,j])
+                    fft_Q = jnp.fft.fft2(field_padded)
+                    convQ = convQ.at[:,:,i,j].set(jnp.fft.ifft2(fft_Q*sp, axes=(0, 1)).real[start_x:start_x + proc.target_shape[0],
+                                        start_y:start_y + proc.target_shape[1]])
+    # return convQ
+    sym = (1/2)*(convQ+np.transpose(convQ,axes=(0, 1, 3, 2)))
+    # return convQ
+    return sym.reshape(-1,4)[proc.rev_DofMap].ravel()
+
+
+def fuq_loss(FE_psi, U, proc):
+    Q = Compute_Q_jax(FE_psi, proc)
+    return jnp.sum((Q - U)**2)
+
+def fuq_loss_sym(FE_psi, U, proc):
+    Q = Compute_Q_sym_jax(FE_psi, proc)
+    return jnp.sum((Q - U)**2)
+
+def jax_computegradFuq(FE_psi,U,proc):
+    k= jax.grad(fuq_loss,argnums=0)(FE_psi,U,proc)
+    return k
+
+
+
+def jax_computegradFuq_sym(FE_psi,U,proc):
+    k= jax.grad(fuq_loss_sym,argnums=0)(FE_psi,U,proc)
+    return k
---
+++ plotfigures/psiondeformedmesh.py
@@ -0,0 +1,105 @@
+import numpy as np
+import meshio
+import pyvista as pv
+from dolfinx.io import XDMFFile
+from mpi4py import MPI
+import h5py
+import dolfinx.plot as plot
+import dolfinx
+import basix
+import dolfinx.fem as fem
+if pv.OFF_SCREEN:
+    pv.start_xvfb(wait=0.1)
+
+
+
+
+def get_im_data(filename,fieldname,ti,comp=None):
+    data1=None
+    X,Y= None,None
+    with h5py.File(filename, "r") as f:
+        try :
+            full_field_timeseries = f["Function/"+fieldname+"/"]
+        except:
+            raise ValueError("Requested field is not available. Available are", list(f["Function"]))
+
+        timekeys= full_field_timeseries.keys()
+        sorted_timesteps = sorted(timekeys, key=lambda s: float(s.replace("_", ".")))
+        # print("exporting data from ", sorted_timesteps[ti])
+        # print("ti = ", ti)
+        if ti >= len(sorted_timesteps):
+            raise ValueError("Requested timestep is not available, maximum snapshot is", len(sorted_timesteps)-1)
+        Full_field = f["Function/"+fieldname+"/"+sorted_timesteps[ti]]
+        mesh= f["Mesh/mesh/geometry"]
+        x = mesh[:, 0]
+        y = mesh[:, 1]
+        nx = len(np.unique(x))
+        ny = len(np.unique(y))
+        newx = np.linspace(x.min(),x.max(),nx)
+        newy = np.linspace(y.min(),y.max(),ny)
+        X,Y = np.meshgrid(newx,newy) 
+        mapping = np.lexsort((mesh[:, 0], mesh[:, 1]))
+        if comp==None:
+            field=Full_field[:]
+        else:
+            field=Full_field[:,comp]
+            
+        data1= field[mapping].reshape(ny, nx)
+    timest = float(sorted_timesteps[ti].replace("_","."))
+    Map = np.copy(mapping)
+    return X,Y,field, timest,Map
+
+
+
+# ---- Step 1: Read the mesh and field data from the XDMF file ----
+file = "./out/indentation/indentation0.2_2.xdmf"
+fileh5 = "./out/indentation/indentation0.2_2.h5"
+with XDMFFile(MPI.COMM_WORLD, file, "r") as xdmf:
+    domain =  xdmf.read_mesh()
+
+
+
+
+
+X,Y,Psi, timest,Map = get_im_data(fileh5,"Psi",2,comp=None)
+X,Y,ux, timest,Map = get_im_data(fileh5,"u",2,comp=0)
+X,Y,uy, timest,Map = get_im_data(fileh5,"u",2,comp=1)
+
+mesh_coords = domain.geometry.x
+Map     = np.lexsort((mesh_coords[:, 0], mesh_coords[:, 1]))
+
+
+rev_DofMap = np.empty_like(Map)
+
+rev_DofMap = rev_DofMap[Map] = np.arange(len(Map))
+
+elem       = basix.ufl.element("Lagrange", domain.basix_cell(), 1)
+Vs_P2      = basix.ufl.element("Lagrange", domain.basix_cell(), 1, shape=(2,))
+main_space = fem.functionspace(domain, elem)
+vectorspace = fem.functionspace(domain, Vs_P2)
+
+psi_func =  fem.Function(main_space,name="Psi")
+u =  fem.Function(vectorspace,name="u")
+
+
+
+u_flat = np.empty((ux.size + uy.size,), dtype=ux.dtype)
+u_flat[0::2] = ux
+u_flat[1::2] = uy
+u.x.array[:] = u_flat
+
+psi_func.x.array[:] = Psi[:,0]
+
+print("dim ",domain.geometry.dim)
+domain.geometry.x[:, :domain.geometry.dim] += u.x.array.reshape((-1, domain.geometry.dim))
+
+cells, types, x = plot.vtk_mesh(domain)
+grid = pv.UnstructuredGrid(cells, types, x)
+
+grid.point_data["psi"] = psi_func.x.array
+grid.set_active_scalars("psi")
+plotter = pv.Plotter(window_size=(750, 400))
+plotter.add_mesh(grid, scalars="psi", cmap="seismic", show_edges=False)
+# plotter.add_scalar_bar("psi")
+plotter.view_xy()
+plotter.show()
