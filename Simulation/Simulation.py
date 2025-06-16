aa7cfe46175ef0dfa8b76a1b594ae1fc78328e29
Aymane.Graini <aymane.graini@polytechnique.edu>
2025-06-16 13:46:05 +0200

    Fixed Q = -d/n..

---
+++ Simulation/Simulation.py
@@ -0,0 +1,15 @@
+
+
+class Simulation:
+    def __init__(self,simparams,geometry):
+        pass
+
+    def Compute_scalars():
+        pass
+
+    def run():
+        pass
+    
+    def write_output():
+        pass
+        
