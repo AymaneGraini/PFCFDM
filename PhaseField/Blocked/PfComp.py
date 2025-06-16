Fix LagrangeMult for average

PhaseField/Blocked/PfComp.py
@@ -10,15 +10,17 @@ class PfComp:
     def __init__(self,
                  pfFe : PFFe):
         self.pfFe = pfFe
+        self.sig  = self.pfFe.pfc_params.a0/122
         self.set_funcs()
-
     def set_funcs(self):
         """
             Defines the main functions computable from ψ like alpha, Q , V_d etc 
         """
-        self.alphaT = fem.Function(self.pfFe.tensor_sp3,name="alphaTild")
-        self.Q = fem.Function(self.pfFe.tensor_sp2,name="Q")
-
+        self.alphaT   = fem.Function(self.pfFe.tensor_sp3,name="alphaTild")
+        self.Q        = fem.Function(self.pfFe.tensor_sp2,name="Q")
+        self.velocity = fem.Function(self.pfFe.vector_sp2,name="Velocity")
+        self.J        = fem.Function(self.pfFe.tensor_sp2,name="J_tens")
+        self.alphaT   = fem.Function(self.pfFe.tensor_sp3,name="alphaTild")
 
     def update_cAmps(self,amps,order):
         for i in range(len(self.pfFe.pfc_params.qs)):
@@ -33,57 +35,57 @@ class PfComp:
 
     def compute_Q(self):
         exp=0
-        for i,q in enumerate(self.qs):
-            D=ufl.grad(self.Im_amps[i])*self.Re_amps[i]- ufl.grad(self.Re_amps[i])*self.Im_amps[i]
+        for i,q in enumerate(self.pfFe.pfc_params.qs):
+            D=ufl.grad(self.pfFe.Im_amps[i])*self.pfFe.Re_amps[i]- ufl.grad(self.pfFe.Re_amps[i])*self.pfFe.Im_amps[i]
             q_field = ufl.as_vector(q) 
-            pref=1/(self.Im_amps[i]**2+self.Re_amps[i]**2)
+            pref=1/(self.pfFe.Im_amps[i]**2+self.pfFe.Re_amps[i]**2)
             exp+=pref*ufl.outer(q_field,D)
-        exp*=2/len(self.qs)
-        self.QT.interpolate(fem.Expression(exp,self.tensor_sp2.element.interpolation_points()))
-        self.alphapfc.interpolate(fem.Expression(tcurl(extendT(self.QT)),self.tensor_sp3.element.interpolation_points()))
+        exp*=2/len(self.pfFe.pfc_params.qs)
+        self.QT.interpolate(fem.Expression(exp,self.pfFe.tensor_sp2.element.interpolation_points()))
+        self.alphapfc.interpolate(fem.Expression(tcurl(extendT(self.QT)),self.pfFe.tensor_sp3.element.interpolation_points()))
 
     def compute_velocityPFC(self):
         self.indicator.interpolate(fem.Expression(ufl.sqrt(self.alphaT[0,2]**2+self.alphaT[1,2]**2),self.scalar_sp.element.interpolation_points()))
         v_exp=0
-        for i,q in enumerate(self.qs):
-            D= ufl.cross(extendV(ufl.grad(self.Re_amps[i])),extendV(ufl.grad(self.Im_amps[i])))
-            Re_dot = (self.Re_amps[i]-self.Re_amps_old[i])/self.pfc_params["dt"]
-            Im_dot = (self.Im_amps[i]-self.Im_amps_old[i])/self.pfc_params["dt"]
-            j= Im_dot*ufl.grad(self.Re_amps[i])-Re_dot*ufl.grad(self.Im_amps[i])
+        for i,q in enumerate(self.pfFe.pfc_params.qs):
+            D= ufl.cross(extendV(ufl.grad(self.pfFe.Re_amps[i])),extendV(ufl.grad(self.pfFe.Im_amps[i])))
+            Re_dot = (self.pfFe.Re_amps[i]-self.pfFe.Re_amps_old[i])/self.pfFe.sim_params.dt
+            Im_dot = (self.pfFe.Im_amps[i]-self.pfFe.Im_amps_old[i])/self.pfFe.sim_params.dt
+            j= Im_dot*ufl.grad(self.pfFe.Re_amps[i])-Re_dot*ufl.grad(self.pfFe.Im_amps[i])
             q_field = ufl.as_vector([q[0],q[1],0])
             b= ufl.dot(self.alphaT,ufl.as_vector([0,0,1]))
             sn = ufl.dot(b/ufl.sqrt(ufl.dot(b,b)),q_field) # a scalar : WInding numer (but it's a density actually)
             v_exp += ((sn**2))*j/D[2]
-        v_exp*=12*np.pi**2/len(self.qs)
-        self.velocity.interpolate(fem.Expression(ufl.conditional(ufl.ge(self.indicator,1e-2),v_exp,ufl.as_vector([0,0])),self.vector_sp2.element.interpolation_points()))
+        v_exp*=12*np.pi**2/len(self.pfFe.pfc_params.qs)
+        self.velocity.interpolate(fem.Expression(ufl.conditional(ufl.ge(self.indicator,1e-2),v_exp,ufl.as_vector([0,0])),self.pfFe.vector_sp2.element.interpolation_points()))
 
     def compute_velocityPFC_bis(self):
-        self.indicator.interpolate(fem.Expression(ufl.sqrt(self.alphaT[0,2]**2+self.alphaT[1,2]**2),self.scalar_sp.element.interpolation_points()))
+        self.indicator.interpolate(fem.Expression(ufl.sqrt(self.alphaT[0,2]**2+self.alphaT[1,2]**2),self.pfFe.scalar_sp.element.interpolation_points()))
         S=0
         D=0
-        for i,q in enumerate(self.qs):
-            d= ufl.cross(extendV(ufl.grad(self.Re_amps[i])),extendV(ufl.grad(self.Im_amps[i])))
-            Re_dot = (self.Re_amps[i]-self.Re_amps_old[i])/self.pfc_params["dt"]
-            Im_dot = (self.Im_amps[i]-self.Im_amps_old[i])/self.pfc_params["dt"]
-            j= Im_dot*ufl.grad(self.Re_amps[i])-Re_dot*ufl.grad(self.Im_amps[i])
-            pref=(1/(2*np.pi*self.sig**2))*ufl.exp(-(self.Re_amps[i]**2+self.Im_amps[i]**2)/(2*self.sig**2))
+        for i,q in enumerate(self.pfFe.pfc_params.qs):
+            d= ufl.cross(extendV(ufl.grad(self.pfFe.Re_amps[i])),extendV(ufl.grad(self.pfFe.Im_amps[i])))
+            Re_dot = (self.pfFe.Re_amps[i]-self.pfFe.Re_amps_old[i])/self.pfFe.sim_params.dt
+            Im_dot = (self.pfFe.Im_amps[i]-self.pfFe.Im_amps_old[i])/self.pfFe.sim_params.dt
+            j= Im_dot*ufl.grad(self.pfFe.Re_amps[i])-Re_dot*ufl.grad(self.pfFe.Im_amps[i])
+            pref=(1/(2*np.pi*self.sig**2))*ufl.exp(-(self.pfFe.Re_amps[i]**2+self.pfFe.Im_amps[i]**2)/(2*self.sig**2))
             S+=3*pref*(q[0]**2+q[1]**2)*j
             D+=d
         v_exp=restrictV(ufl.cross(extendV(j),D)/ufl.dot(D,D))
-        self.velocity.interpolate(fem.Expression(ufl.conditional(ufl.ge(self.indicator,1e-2),v_exp,ufl.as_vector([0,0])),self.vector_sp2.element.interpolation_points()))
+        self.velocity.interpolate(fem.Expression(ufl.conditional(ufl.ge(self.indicator,1e-2),v_exp,ufl.as_vector([0,0])),self.pfFe.vector_sp2.element.interpolation_points()))
 
     def Compute_current(self):
-        if self.pfc_params['motion']=="up":
+        if self.pfFe.pfc_params.motion=="up":
             exp=0
-            for i,q in enumerate(self.qs):
-                Re_dot = (self.Re_amps[i]-self.Re_amps_old[i])/self.pfc_params["dt"]
-                Im_dot = (self.Im_amps[i]-self.Im_amps_old[i])/self.pfc_params["dt"]
-                j= Im_dot*ufl.grad(self.Re_amps[i])-Re_dot*ufl.grad(self.Im_amps[i])
+            for i,q in enumerate(self.pfFe.pfc_params.qs):
+                Re_dot = (self.pfFe.Re_amps[i]-self.pfFe.Re_amps_old[i])/self.pfFe.sim_params.dt
+                Im_dot = (self.pfFe.Im_amps[i]-self.pfFe.Im_amps_old[i])/self.pfFe.sim_params.dt
+                j= Im_dot*ufl.grad(self.pfFe.Re_amps[i])-Re_dot*ufl.grad(self.pfFe.Im_amps[i])
                 q_field = ufl.as_vector([q[0],q[1]]) 
-                pref=(1/(2*np.pi*self.sig**2))*ufl.exp(-(self.Re_amps[i]**2+self.Im_amps[i]**2)/(2*self.sig**2))
+                pref=(1/(2*np.pi*self.sig**2))*ufl.exp(-(self.pfFe.Re_amps[i]**2+self.pfFe.Im_amps[i]**2)/(2*self.sig**2))
                 exp+=pref*ufl.outer(q_field,j)
-            exp*=(2*3*np.pi)/len(self.qs)
-        elif self.pfc_params['motion']=="v":
+            exp*=(2*3*np.pi)/len(self.pfFe.pfc_params.qs) #TODO -1 ?
+        elif self.pfFe.pfc_params.motion=="v":
             print("Updating with velocity")
             self.compute_velocityPFC_bis()
             exp = restrictT(tcrossv(self.alphaT,extendV(self.velocity)))
@@ -91,17 +93,17 @@ class PfComp:
         else:
             raise ValueError("Not implemented way of updating UP")
         
-        self.J.interpolate(fem.Expression(exp,self.tensor_sp2.element.interpolation_points()))
+        self.J.interpolate(fem.Expression(exp,self.pfFe.tensor_sp2.element.interpolation_points()))
 
     def Compute_alpha_tilde(self):
         exp=0
-        for i,q in enumerate(self.qs):
-            D= ufl.cross(extendV(ufl.grad(self.Re_amps[i])),extendV(ufl.grad(self.Im_amps[i])))
+        for i,q in enumerate(self.pfFe.pfc_params.qs):
+            D= ufl.cross(extendV(ufl.grad(self.pfFe.Re_amps[i])),extendV(ufl.grad(self.pfFe.Im_amps[i])))
             q_field = ufl.as_vector([q[0],q[1],0]) 
-            pref=(1/(2*np.pi*self.sig**2))*ufl.exp(-(self.Re_amps[i]**2+self.Im_amps[i]**2)/(2*self.sig**2))
+            pref=(1/(2*np.pi*self.sig**2))*ufl.exp(-(self.pfFe.Re_amps[i]**2+self.pfFe.Im_amps[i]**2)/(2*self.sig**2))
             exp+=pref*ufl.outer(q_field,D)
-        exp*=(2*3*np.pi)/len(self.qs)
-        self.alphaT.interpolate(fem.Expression(exp,self.tensor_sp3.element.interpolation_points()))
+        exp*=(2*3*np.pi)/len(self.pfFe.pfc_params.qs)
+        self.alphaT.interpolate(fem.Expression(exp,self.pfFe.tensor_sp3.element.interpolation_points()))
 
 
     def Compute_microscopic_stress(self):

PhaseField/Blocked/PfFe.py
@@ -13,7 +13,7 @@ from utils.utils import *
 import scipy.ndimage as ndimage
 import jax.numpy as jnp
 from Simulation.Parameters import*
-
+import scifem
 
 
 
@@ -47,7 +47,7 @@ class PFFe:
 
         self.Vs_P2      = basix.ufl.element("Lagrange", self.domain.basix_cell(), 1,shape=(2,))
         self.vector_sp2 = fem.functionspace(self.domain, self.Vs_P2)
-
+        self.real_space = scifem.create_real_functionspace(self.domain)
 
     def set_funcs(self):
         """

PhaseField/Mixed/PfComp.py
@@ -84,7 +84,7 @@ class PfComp:
                 q_field = ufl.as_vector([q[0],q[1]]) 
                 pref=(1/(2*np.pi*self.sig**2))*ufl.exp(-(self.pfFe.Re_amps[i]**2+self.pfFe.Im_amps[i]**2)/(2*self.sig**2))
                 exp+=pref*ufl.outer(q_field,j)
-            exp*=(2*3*np.pi)/len(self.pfFe.pfc_params.qs)
+            exp*=(2*3*np.pi)/len(self.pfFe.pfc_params.qs) #TODO - or +?
         elif self.pfFe.pfc_params.motion=="v":
             print("Updating with velocity")
             self.compute_velocityPFC_bis()

Static simulation.py
@@ -1,5 +1,5 @@
-import PhaseField.Mixed as Mixed
-import PhaseField.Mixed.PfProc
+import PhaseField.Blocked as Blocked
+from PhaseField.Blocked import PfProc
 import Mechanics
 from Simulation.Parameters import *
 from Simulation.SimIO import *
@@ -17,6 +17,7 @@ import pyvista
 from dolfinx.la import create_petsc_vector_wrap
 from PFCproc_TODO.ProcessPFC_padFFT import *
 from jax import vjp, jvp
+
 
 if pyvista.OFF_SCREEN:
     pyvista.start_xvfb(wait=0.1)
@@ -41,11 +42,11 @@ geometry   = GeomParams(dx=pfcparms.a0/7,
                         Ny=12*22) # the domain size should the multiple of 12 for periodicity of e^(iq.x)
 
 
-simparams = SimParams(1,1,True,True,1e-1,10,geometry.L,geometry.H)
+simparams = SimParams(1,5,True,True,1e-1,50,geometry.L,geometry.H)
 
 
-filename  = "Static"+str(simparams.dt)+"_"+str(simparams.Cw)
-path = "./out/Static/periodic/penaltyMec/"
+filename  = "static"+str(simparams.dt)+"_"+str(simparams.Cw)
+path = "./out/Static/"
 file = dolfinx.io.XDMFFile(MPI.COMM_WORLD, path+filename+".xdmf", "w")
 
 
@@ -92,11 +93,10 @@ defects=[
 
 
 
-
 timestamps=[0]
 SH_Energy = []
 
-pfProc = Mixed.PfProc.PfProc(domain,pfcparms,simparams,file)
+pfProc = PfProc.PfProc(domain,pfcparms,simparams,file)
 pfProc.Initialize_crystal(defects)
 pfProc.init_solver()
 pfProc.Configure_solver()
@@ -145,6 +145,8 @@ component_errors_rel = [
     error_L2_rel(mec_proc.mecFE.UEsym.sub(i), mec_proc.mecComp.Qsym.sub(i))
     for i in range(4)
 ]
+avg_history=[fem.assemble_scalar(pfProc.pfFe.Avg_form)]
+
 erros_history=[component_errors]
 mechanical_dissipation=[0]
 rel_erros_history=[component_errors_rel]
@@ -165,8 +167,6 @@ while t<simparams.tmax:
     mec_proc.mecFE.Q.x.array[:]=Proc.Compute_Q(amps)
     mec_proc.mecFE.alpha.x.array[:]=Proc.Compute_alpha(mec_proc.mecFE.Q.x.array)
     mec_proc.update_UP(pfProc)
-    # mec_proc.solveUPperp()
-    # mec_proc.combine_UP()
     mec_proc.solveU()
     mec_proc.extract_UE()  
     mec_proc.compute_sym()
@@ -176,6 +176,8 @@ while t<simparams.tmax:
     component_errors = [error_L2(mec_proc.mecFE.UEsym.sub(i), mec_proc.mecComp.Qsym.sub(i)) for i in range(4)]
     erros_history.append(component_errors)
     rel_erros_history.append([error_L2_rel(mec_proc.mecFE.UEsym.sub(i), mec_proc.mecComp.Qsym.sub(i)) for i in range(4)])
+    avg_history.append(fem.assemble_scalar(pfProc.pfFe.Avg_form))
+
     timestamps.append(t)
     # pfProc.write_output(t)
     # mec_proc.write_output(t)
@@ -190,6 +192,6 @@ mec_proc.write_output(t)
 
 
 file.close()
-np.savetxt(path+"Energy_"+filename+".csv",np.column_stack((timestamps,SH_Energy,mechanical_dissipation)),delimiter="\t")
+np.savetxt(path+"Energy_"+filename+".csv",np.column_stack((timestamps,SH_Energy,mechanical_dissipation,avg_history)),delimiter="\t")
 np.savetxt(path+"errors_"+filename+".csv",np.column_stack((timestamps,erros_history)),delimiter="\t")
 